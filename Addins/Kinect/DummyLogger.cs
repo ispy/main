10f21c00172ccb3db1d6197913ce4d517169fe85
Eric Newton <ericnewton76@gmail.com>
2013-02-27 22:11:05 -0500

    Created the KinectAddin project and moved KinectNetworkStream and KinectStream in

---
+++ Addins/Kinect/DummyLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Logging
+{
+	internal interface ILog
+	{
+		void WarnFormat(string format, params object[] args);
+		void Error(Exception ex);
+	}
+	
+	internal class DummyLogger : Common.Logging.ILog
+	{
+		public void WarnFormat(string format, params object[] args) {}
+		public void Error(Exception ex) {}
+	}
+
+	internal static class LogManager
+	{
+		public static Common.Logging.ILog GetLoggerForCurrentClass()
+		{
+			return new DummyLogger();
+		}
+	}
+}
